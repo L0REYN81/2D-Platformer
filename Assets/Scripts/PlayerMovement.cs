19a1fb09f7ff743168360c5eedf9ca8321712c17
LOREYN81 <programmer.pin23@gmail.com>
2026-04-05 23:29:55 +0300

    Add Double Jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private int maxJumps = 2;
+
+    private Rigidbody2D body;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    private float wallJumpCooldown;
+    private float horizontalInput;
+    private int jumpCount;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Движение
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        // Поворот
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(4, 4, 4);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-4, 4, 4);
+
+        // Сброс прыжков
+        if (isGrounded() && body.linearVelocity.y <= 0)
+        {
+            jumpCount = 0;
+        }
+
+        // Прыжок
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
+        // Анимации
+        animator.SetBool("run", horizontalInput != 0);
+        animator.SetBool("grounded", isGrounded());
+        animator.SetFloat("yVelocity", body.linearVelocity.y);
+
+        // Прыжок от стены
+        if (wallJumpCooldown > 0.2f)
+        {
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else
+                body.gravityScale = 7;
+        }
+        else
+            wallJumpCooldown += Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        // Прыжок с земли
+        if (isGrounded())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+            animator.SetTrigger("jump");
+            jumpCount = 1;
+        }
+        // Прыжок от стены
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                body.linearVelocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+
+            wallJumpCooldown = 0;
+        }
+        // Двойной прыжок
+        else if (jumpCount < maxJumps)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+            animator.SetTrigger("doubleJump");
+            jumpCount++;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0,
+            Vector2.down,
+            0.1f,
+            groundLayer
+        );
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0,
+            new Vector2(transform.localScale.x, 0),
+            0.1f,
+            wallLayer
+        );
+        return raycastHit.collider != null;
+    }
+}
